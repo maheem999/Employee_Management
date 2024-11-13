94db2ea6503ab52bea066304258f69aa3f23a44e
maheem999 <105554566+maheem999@users.noreply.github.com>
2024-11-13 20:54:32 +0600

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+$servername ="localhost";
+$username="root";
+$password="";
+$dbname="company_management";
+
+$connection=new mysqli($servername, $username,$password,$dbname);
+
+if($connection->connect_error)
+{
+    echo "fail to conect databse";
+}
+?>
